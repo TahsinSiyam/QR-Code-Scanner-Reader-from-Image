7430a016ae2b153d425888edd31716d4f0dfbf46
Tahsin Siyam <tahsinsiyam13@gmail.com>
2026-04-03 15:22:07 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+import qrcode as qr
+
+
+inp = input("1. Encode                2. Decode")
+
+
+def createQR():
+    try:
+        data = input("Enter Data: ")
+        img = qr.make(data=data)
+        img.save("qrcode.png")
+    except:
+        print("An Error Occured!")
+
+
+def scanQR():
+    try:
+        img = input("Enter Image path here: ")
+        imgReader = cv2.imread(f'r"{img}"')
+        detector = cv2.QRCodeDetector
+        data, bbox, staright_code = detector.detectAndDecode(img=imgReader)
+        if data:
+            print(f"Decoded Data: {data}")
+        else:
+            print("Something went Wrong!")
+
+    except :
+        print("Error Occured!")
+
+if inp == 1:
+    createQR()
+elif inp == 2:
+    scanQR()
+else:
+    print("An Error Occured!")
